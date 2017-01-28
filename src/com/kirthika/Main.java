eff55d2da436e6906dfa806f3cc4fbb530bd106f
Kirthika S <kirthika05@gmail.com>
2017-01-27 21:28:17 -0600

    Initial commit

---
+++ src/com/kirthika/Main.java
@@ -0,0 +1,67 @@
+package com.kirthika;
+
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+
+}
+
+public class Main {
+
+    public static Node removeDuplicates(Node head){
+        Node currentNode = head;
+        while(currentNode!=null && currentNode.next!=null){
+            while(currentNode.next!=null && currentNode.data==currentNode.next.data){
+                currentNode.next = currentNode.next.next;
+            }
+            currentNode=currentNode.next;
+        }
+        return head;
+    }
+
+    public static  Node insert(Node head,int data)
+    {
+        Node p=new Node(data);
+        if(head==null)
+            head=p;
+        else if(head.next==null)
+            head.next=p;
+        else
+        {
+            Node start=head;
+            while(start.next!=null)
+                start=start.next;
+            start.next=p;
+
+        }
+        return head;
+    }
+    public static void display(Node head)
+    {
+        Node start=head;
+        while(start!=null)
+        {
+            System.out.print(start.data+" ");
+            start=start.next;
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc=new Scanner(System.in);
+        Node head=null;
+        int T=sc.nextInt();
+        while(T-->0){
+            int ele=sc.nextInt();
+            head=insert(head,ele);
+        }
+        head=removeDuplicates(head);
+        display(head);
+    }
+}
